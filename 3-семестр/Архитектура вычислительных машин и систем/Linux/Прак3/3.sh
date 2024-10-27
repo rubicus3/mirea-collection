1fa7eb1fd00312204cb44ded33c351fae5639b69
rubicus3 <laba625@yandex.ru>
2024-10-28 00:14:15 +0300

    Fix linux 3 2-3-4

--- 3-семестр/Архитектура вычислительных машин и систем/Linux/Прак3/3.sh
+++ 3-семестр/Архитектура вычислительных машин и систем/Linux/Прак3/3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ls -lR $1 | awk 'BEGIN { sum = 0 } $1 ~ /^-/ && $9 ~ /\.txt$/ {sum += $5} END { print "Сумма =", sum }'
+ls -lR $1 | awk '
+BEGIN { sum = 0 } $1 ~ /^-/ && $9 ~ /\.txt$/ {sum += $5}
+END { print "Сумма байтов, занятых txt-файлами =", sum }'
--- 3-семестр/Архитектура вычислительных машин и систем/Linux/Прак3/4.sh
+++ 3-семестр/Архитектура вычислительных машин и систем/Linux/Прак3/4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -la --block-size=4K $1 | awk 'BEGIN { sum = 0 } $1 ~ /^-/ {sum += $5} END { print "Кол-во = ", sum }'
+ls -ls $1 | awk 'BEGIN { sum = 0 } {sum += $1} END { print "Сумма каталога в блоках = ", sum }'
